0a17154f9c5f4fc1ea36d45ead84f68803790214
njugus <kayvohnjuguna@gmail.com>
2024-10-07 21:34:38 -0700

    sets up the development enviroment for using numpy

---
+++ ArrayFundamentals.py
@@ -0,0 +1,9 @@
+# working with numpy to perform numerical/scientific operations on arrays
+# three main characteristics of numpy arrays
+# 1.) Homogenous Arrays
+# 2.) You cannot add or remove elements from these arrays as you can do with lists but you can modify an array.
+# 3.) Rectangular not jagged arrays - every row should have similar number of columns.
+
+
+# initialize an array using a list
+import numpy as np
