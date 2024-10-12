3213a317a4d9847addbc1e8e7dd6032adc35ccd6
njugus <kayvohnjuguna@gmail.com>
2024-10-12 10:59:56 -0700

    adds random generation of elements in numpy arrays using random module

---
+++ ArrayNoteBetters.py
@@ -0,0 +1,9 @@
+# For testing purposes it is often necessary to generate random arrays
+# how to generate random arrays
+
+import numpy as np
+
+random_number_array = np.random.randint(0, 10, 3)
+print(random_number_array)
+
+
--- trials.py
+++ trials.py
@@ -1,9 +1,2 @@
 import numpy as np
 
-# printing arrays
-# 1-D arrays
-numbers_arrays = np.arange(12)
-
-# printing 2-D arrays
-new_array = np.arange(12).reshape(4, 3)
-print(new_array)
